4474371fac19bd83edb11e82f73ab31f45129fa8
nsellek <nick@seansellek.com>
2016-06-16 18:46:53 -0400

    assigned elements to to the login controller prototype

--- www/js/login-controller.js
+++ www/js/login-controller.js
@@ -9,5 +9,9 @@ Staze.LogInController = function(){
 }
 
 Staze.LogInController.prototype.init = function(){
-  
+  this.$logInPage = $("#page-login");
+  this.$logInBtnSubmit = $("#login-btn-submit", this.$logInPage);
+  this.$logInEmail = $("#login-email", this.$logInPage);
+  this.$logInPass = $("#login-password", this.$logInPage);
+  this.logInView = $("#loged-in-view");
 }
