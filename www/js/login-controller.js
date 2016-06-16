56bfeb11a71a0a9d71aae4fdf6f2895e9a4f22a9
nsellek <nick@seansellek.com>
2016-06-16 18:39:23 -0400

    setting up Staze login contoller

--- www/js/login-controller.js
+++ www/js/login-controller.js
@@ -1,1 +1,13 @@
 var Staze = Staze || {};
+
+Staze.LogInController = function(){
+  this.$logInPage = null;
+  this.$logInBtnSubmit = null;
+  this.$logInEmail = null;
+  this.$logInPass = null;
+  this.logInView = null;
+}
+
+Staze.LogInController.prototype.init = function(){
+  
+}
